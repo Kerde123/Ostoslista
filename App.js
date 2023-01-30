26adba9a622158f08accb4bee4d2db964d190ef8
Kerde123 <kerttu.vanhala@gmail.com>
2023-01-30 19:31:11 +0200

    Valmis

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+
+  const buttonPressed = () => {
+    setData([...data, { key: text }]);
+    setText('');
+  }
+
+  const buttonCleared = () => {
+    setData([]);
+    setText('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput style={styles.input} onChangeText={text => setText(text)} value={text} />
+      <View style={styles.fixToText}>
+      <Button onPress={buttonPressed} title="ADD" />
+      <Button onPress={buttonCleared} title="CLEAR" />
+      </View>
+      <Text></Text>
+      <Text style={styles.title}>Shopping List</Text>
+      <FlatList style={styles.list}
+        data={data}
+        renderItem={({ item }) =>
+          <Text>{item.key}</Text>
+        }
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +41,21 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    marginTop: 200,
   },
+  input: {
+    marginTop: 50,
+    marginBottom: 5,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1 
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  title: {
+    color: 'blue', 
+  }
 });
